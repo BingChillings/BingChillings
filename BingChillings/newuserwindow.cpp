0278f39e274b93ba3341153684d41725229971c3
xxsyang <111765058+xxsyang@users.noreply.github.com>
2023-04-20 17:35:12 -0600

    done check, waiting for users Qvector

--- BingChillings/newuserwindow.cpp
+++ BingChillings/newuserwindow.cpp
@@ -9,9 +9,102 @@ NewUserWindow::NewUserWindow(QWidget *parent) :
     ui->genderComboBox->addItem("Male");
     ui->genderComboBox->addItem("Female");
     ui->genderComboBox->addItem("Other");
+
+    firstName = ui->firstNameEdit->text();
+    lastName = ui->lastNameEdit->text();
+    dateBirth = ui->birthdayEdit->date();
+    gender = ui->genderComboBox->currentText();
+    profilePicPath = "xxxx";
+    username = ui->usernameEdit->text();
+    password = ui->passwordEdit->text();
+
+
 }
 
 NewUserWindow::~NewUserWindow()
 {
     delete ui;
+}
+
+bool NewUserWindow::validateUsername() {
+    bool isValidate = false;
+    if (username.length() < 3) {
+//        throw std::runtime_error("Username must be at least 3 characters long.");
+        return isValidate;
+    }
+
+    bool hasInvalidCharacter = false;
+
+    for (QChar &ch : username) {
+        if (!ch.isLetterOrNumber() || ch.isSpace()) {
+            hasInvalidCharacter = true;
+            break;
+        }
+    }
+
+    if (hasInvalidCharacter) {
+//        throw std::runtime_error("Username must not contain special characters or spaces.");
+        return isValidate;
+    }
+
+    return true;
+
+}
+
+bool NewUserWindow::validatePassword(){
+    if (password.length() < 8)
+        throw std::runtime_error("Password must be at least 8 characters long.");
+
+    bool hasCapitalLetter = false;
+    bool hasSpecialCharacter = false;
+    bool hasSpace = false;
+
+    for (QChar &ch : password) {
+        if (ch.isUpper())
+            hasCapitalLetter = true;
+
+        if (ch.isSpace())
+            hasSpace = true;
+
+        if (!ch.isLetterOrNumber())
+            hasSpecialCharacter = true;
+    }
+
+    // True: sign-up
+    // False: Login
+    if (signupOrLogin){
+        // Write error message to to sign-up page
+        if (!hasCapitalLetter)
+            throw std::runtime_error("Password must have at least one capital letter.");
+
+        if (!hasSpecialCharacter)
+            throw std::runtime_error("Password must have at least one special character.");
+
+        if (hasSpace)
+            throw std::runtime_error("Password must not contain any spaces.");
+    }
+    else {
+        // Write error message to sign up page
+        if (!hasCapitalLetter)
+            throw std::runtime_error("Password must have at least one capital letter.");
+
+        if (!hasSpecialCharacter)
+            throw std::runtime_error("Password must have at least one special character.");
+
+        if (hasSpace)
+            throw std::runtime_error("Password must not contain any spaces.");
+    }
+
+
+}
+
+void NewUserWindow::setNewUserInfo()
+{
+    if(validatePassword() == true && validateUsername() == true){
+        User* newUser = new User(firstName, lastName, dateBirth, gender, profilePicPath, username, password, emptyVector, true);
+
+    }
+
+
+
 }
